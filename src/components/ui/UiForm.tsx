4bcda361cc5be0413e8e394d884f9071ff53f5c9
god-of-js <henryeze019@gmail.com>
2023-01-25 23:01:50 +0100

    chore(): figuring out form concept

--- src/components/ui/UiForm.tsx
+++ src/components/ui/UiForm.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import { Formik } from 'formik';
 
+const ruleCheck = {
+  required: true,
+}
+
 interface Props {
-  rules: Record<string, unknown[]>;
+  rules: Record<string, string[]>;
   formData: Record<string, any>;
+  children?: React.ReactNode;
   onSubmit: () => void;
 }
 
-export default function UiForm({ rules, formData, onSubmit }: Props) {
-  function validateForm() {
-    const errors = {};
-    
+export default function UiForm({ rules, formData, children, onSubmit }: Props) {
+  function validateForm(values: Record<string, string>) {
+    const errors: Record<string, string> = {};
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+    ) {
+      errors.email = 'Invalid email address';
+    }
+    return errors;
   }
 
-  function handleSubmit() {}
+  function handleSubmit() { }
 
   return (
     <Formik
@@ -21,7 +33,15 @@ export default function UiForm({ rules, formData, onSubmit }: Props) {
       validate={validateForm}
       onSubmit={handleSubmit}
     >
-
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      }) => <form>{children}</form>}
     </Formik>
   );
 }
