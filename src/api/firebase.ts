0bdce0989257988778ea44a941245b5ef5b06191
god-of-js <henryeze019@gmail.com>
2023-03-11 21:39:41 +0100

    chore(): prod database configured

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -21,16 +21,6 @@ if (process.env.NODE_ENV === 'production') {
     messagingSenderId: "74982091969",
     appId: "1:74982091969:web:4953ce83c4de1731073840",
     measurementId: "G-7QEDX9F755"
-  };;
-} else {
-  firebaseConfig = {
-    apiKey: 'AIzaSyCseX2pxV3j0FUYBCt1ifNto5Fdj0fpuW4',
-    authDomain: 'td-dev-c20a2.firebaseapp.com',
-    projectId: 'td-dev-c20a2',
-    storageBucket: 'td-dev-c20a2.appspot.com',
-    messagingSenderId: '926041702630',
-    appId: '1:926041702630:web:4ca168cb3f90d7f0527b27',
-    measurementId: 'G-M2LDKSM2BT',
   };
 }
 
