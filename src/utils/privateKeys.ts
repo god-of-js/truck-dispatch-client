0c0f6cb57b6d3ce9b14baa4aa69ffb2444af995e
god-of-js <henryeze019@gmail.com>
2023-03-14 01:13:35 +0100

    chore(): secrets added to environment

--- src/utils/privateKeys.ts
+++ src/utils/privateKeys.ts
@@ -1,3 +1,6 @@
-export const VITE_PAYSTACK_PUBLIC_KEY = import.meta.env.VITE_PAYSTACK_PRIVATE_KEY;
+export const paystackPublickKey = import.meta.env.VITE_PAYSTACK_PRIVATE_KEY;
 
 export const paystackPrivateKey =import.meta.env.VITE_PAYSTACK_PRIVATE_KEY;
+
+export const CLOUDINARY_IMAGE_UPLOAD_URL = import.meta.env.VITE_CLOUDINARY_IMAGE_UPLOAD_URL
+export const CLOUDINARY_VIDEO_UPLOAD_URL = import.meta.env.VITE_CLOUDINARY_VIDEO_UPLOAD_URL
