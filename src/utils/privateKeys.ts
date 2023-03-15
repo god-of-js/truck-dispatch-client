54928fe4300a45156321b366f38e48750572bfbd
god-of-js <henryeze019@gmail.com>
2023-03-15 02:06:16 +0100

    test

--- src/utils/privateKeys.ts
+++ src/utils/privateKeys.ts
@@ -1,7 +1,7 @@
 const isDev = import.meta.env.MODE === 'development'
 export const paystackPublickKey = import.meta.env.VITE_PAYSTACK_PRIVATE_KEY;
 
-export const paystackPrivateKey = isDev ? import.meta.env.VITE_PAYSTACK_PRIVATE_KEY : import.meta.env.VERCEL_VITE_CLOUDINARY_VIDEO_UPLOAD_URL;
+export const paystackPrivateKey = isDev ? import.meta.env.VITE_PAYSTACK_PRIVATE_KEY : import.meta.env.VERCEL_CLOUDINARY_VIDEO_UPLOAD_URL;
 
 export const CLOUDINARY_IMAGE_UPLOAD_URL = import.meta.env.VITE_CLOUDINARY_IMAGE_UPLOAD_URL
 export const CLOUDINARY_VIDEO_UPLOAD_URL = import.meta.env.VITE_CLOUDINARY_VIDEO_UPLOAD_URL
