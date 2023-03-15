f704b5d7a4922d2e582fd4724a86552e069ba55f
god-of-js <henryeze019@gmail.com>
2023-03-15 23:59:21 +0100

    renames

--- src/utils/privateKeys.ts
+++ src/utils/privateKeys.ts
@@ -1,8 +1,8 @@
 
 const isDev = import.meta.env.MODE === 'development';
-export const paystackPublickKey = isDev ? import.meta.env.VITE_PAYSTACK_PUBLIC_KEY : process.env.VITE_PAYSTACK_PUBLIC_KEY;
+export const paystackPublickKey = import.meta.env.VITE_PAYSTACK_PUBLIC_KEY;
 
-export const paystackPrivateKey = isDev ? import.meta.env.VITE_PAYSTACK_PRIVATE_KEY : process.env.VITE_PAYSTACK_PRIVATE_KEY;
+export const paystackPrivateKey = import.meta.env.VITE_PAYSTACK_PRIVATE_KEY;
 
-export const CLOUDINARY_IMAGE_UPLOAD_URL = isDev ? import.meta.env.VITE_CLOUDINARY_IMAGE_UPLOAD_URL : process.env.VITE_CLOUDINARY_IMAGE_UPLOAD_URL;
-export const CLOUDINARY_VIDEO_UPLOAD_URL = isDev ? import.meta.env.VITE_CLOUDINARY_VIDEO_UPLOAD_URL : process.env.VITE_CLOUDINARY_VIDEO_UPLOAD_URL ;
+export const CLOUDINARY_IMAGE_UPLOAD_URL = import.meta.env.VITE_CLOUDINARY_IMAGE_UPLOAD_URL;
+export const CLOUDINARY_VIDEO_UPLOAD_URL = import.meta.env.VITE_CLOUDINARY_VIDEO_UPLOAD_URL;
