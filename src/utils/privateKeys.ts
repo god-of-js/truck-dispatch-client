aba60a7af56985df062f476bf652a10448a36524
god-of-js <henryeze019@gmail.com>
2023-03-15 22:55:41 +0100

    feat(): vite vercel prefix added

--- src/utils/privateKeys.ts
+++ src/utils/privateKeys.ts
@@ -3,5 +3,5 @@ export const paystackPublickKey = import.meta.env.VITE_PAYSTACK_PRIVATE_KEY;
 
 export const paystackPrivateKey = isDev ? import.meta.env.VITE_PAYSTACK_PRIVATE_KEY : import.meta.env.VITE_VERCEL_CLOUDINARY_VIDEO_UPLOAD_URL;
 
-export const CLOUDINARY_IMAGE_UPLOAD_URL = import.meta.env.VITE_VERCEL_ENV_CLOUDINARY_IMAGE_UPLOAD_URL;
+export const CLOUDINARY_IMAGE_UPLOAD_URL = import.meta.env.VITE_VERCEL_CLOUDINARY_IMAGE_UPLOAD_URL;
 export const CLOUDINARY_VIDEO_UPLOAD_URL = import.meta.env.REACT_APP_VERCEL_ENV_CLOUDINARY_VIDEO_UPLOAD_URL;
