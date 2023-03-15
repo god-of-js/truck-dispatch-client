a97ece2e54c4935b4c18d0a3a78c63837e76edd5
god-of-js <henryeze019@gmail.com>
2023-03-15 23:39:46 +0100

    chore(): added VITE_PAYSTACK_PUBLIC_KEY

--- src/utils/privateKeys.ts
+++ src/utils/privateKeys.ts
@@ -1,6 +1,6 @@
 
 const isDev = import.meta.env.MODE === 'development';
-export const paystackPublickKey = isDev ? import.meta.env.VITE_PAYSTACK_PUBLIC_KEY : process.env;
+export const paystackPublickKey = isDev ? import.meta.env.VITE_PAYSTACK_PUBLIC_KEY : process.env.VITE_PAYSTACK_PUBLIC_KEY;
 
 export const paystackPrivateKey = isDev ? import.meta.env.VITE_PAYSTACK_PRIVATE_KEY : process.env.VITE_PAYSTACK_PRIVATE_KEY;
 
