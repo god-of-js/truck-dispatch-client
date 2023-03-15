dbfc5c1879d8d5c24b905a51308c24462bf1b6ec
god-of-js <henryeze019@gmail.com>
2023-03-15 02:27:38 +0100

    feat(): reload

--- src/utils/privateKeys.ts
+++ src/utils/privateKeys.ts
@@ -4,4 +4,4 @@ export const paystackPublickKey = import.meta.env.VITE_PAYSTACK_PRIVATE_KEY;
 export const paystackPrivateKey = isDev ? import.meta.env.VITE_PAYSTACK_PRIVATE_KEY : import.meta.env.VITE_VERCEL_CLOUDINARY_VIDEO_UPLOAD_URL;
 
 export const CLOUDINARY_IMAGE_UPLOAD_URL = import.meta.env.VITE_VERCEL_ENV_CLOUDINARY_IMAGE_UPLOAD_URL
-export const CLOUDINARY_VIDEO_UPLOAD_URL = import.meta.env.VITE_CLOUDINARY_VIDEO_UPLOAD_URL
+export const CLOUDINARY_VIDEO_UPLOAD_URL = import.meta.env.REACT_APP_VERCEL_ENV_CLOUDINARY_VIDEO_UPLOAD_URL
