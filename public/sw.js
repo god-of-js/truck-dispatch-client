9d0ef424b37dd8599989976952b3f7d486143bf3
god-of-js <henryeze019@gmail.com>
2023-03-16 14:58:55 +0100

    chore(): cache service worker added

---
+++ public/sw.js
@@ -0,0 +1,43 @@
+// In your service worker file
+
+const CACHE_NAME = "my-cache-v1";
+const urlsToCache = ["/index.html", "/static/js/bundle.js", "/static/css/main.css"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      // Clone the request because it's a stream that can only be consumed once
+      const fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then((response) => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        // Clone the response because it's a stream that can only be consumed once
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
