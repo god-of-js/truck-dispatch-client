90498ea8b755678bf16692147cb5667366991159
Onyewuchi Emeka <marrnuel123@.com>
2023-05-03 13:27:54 +0100

    organized the icons accordingly

--- src/components/ui/UiIcon.tsx
+++ src/components/ui/UiIcon.tsx
@@ -29,25 +29,25 @@ import {
 } from 'phosphor-react';
 import { ReactComponent as ArrowCircleLeft } from './icons/arrow-circle-left.svg';
 import { ReactComponent as ArrowCircleRight } from './icons/arrow-circle-right.svg';
+import { ReactComponent as CallReceived } from './icons/call-received.svg';
+import { ReactComponent as Car } from './icons/car.svg';
 import { ReactComponent as ChartSquare } from './icons/chart-square.svg';
+import { ReactComponent as Check } from './icons/check.svg';
+import { ReactComponent as Company } from './icons/company.svg';
+import { ReactComponent as DocumentUpload } from './icons/document-upload.svg';
+import { ReactComponent as Eye } from './icons/eye.svg';
+import { ReactComponent as EyeSlash } from './icons/eye-slash.svg';
 import { ReactComponent as Jobs } from './icons/jobs.svg';
+import { ReactComponent as InfoCircle } from './icons/info-circle.svg';
 import { ReactComponent as Logout } from './icons/log-out.svg';
+import { ReactComponent as MagicStar } from './icons/magic-star.svg';
 import { ReactComponent as MessageChat } from './icons/chat.svg';
 import { ReactComponent as Moneys } from './icons/moneys.svg';
+import { ReactComponent as PasswordCheck } from './icons/password-check.svg';
 import { ReactComponent as Settings } from './icons/settings.svg';
 import { ReactComponent as TruckImg } from './icons/truck.svg';
 import { ReactComponent as TruckTick } from './icons/ticked-truck.svg';
-import { ReactComponent as Company } from './icons/company.svg';
-import { ReactComponent as Car } from './icons/car.svg';
-import { ReactComponent as Check } from './icons/check.svg';
-import { ReactComponent as CallReceived } from './icons/call-received.svg';
-import { ReactComponent as DocumentUpload } from './icons/document-upload.svg';
-import { ReactComponent as Eye } from './icons/eye.svg';
 import { ReactComponent as User } from './icons/user.svg';
-import { ReactComponent as EyeSlash } from './icons/eye-slash.svg';
-import { ReactComponent as InfoCircle } from './icons/info-circle.svg';
-import { ReactComponent as MagicStar } from './icons/magic-star.svg';
-import { ReactComponent as PasswordCheck } from './icons/password-check.svg';
 import { ReactComponent as UserOctagon } from './icons/user-octagon.svg';
 import { ReactComponent as UserSquare } from './icons/user-square.svg';
 
@@ -65,9 +65,9 @@ const icons = {
   CaretUp: <CaretUp />,
   Chats: <Chats />,
   ChartSquare: <ChartSquare />,
-  CreditCard: <CreditCard />,
   Check: <Check />,
   Checks: <Checks />,
+  CreditCard: <CreditCard />,
   DocumentUpload: <DocumentUpload />,
   DotsThree: <DotsThree />,
   DotsThreeVertical: <DotsThreeVertical />,
@@ -82,8 +82,8 @@ const icons = {
   MagicStar: <MagicStar />,
   MapPin: <MapPin />,
   MessageChat: <MessageChat />,
-  Moneys: <Moneys />,
   Money: <Money />,
+  Moneys: <Moneys />,
   PaperPlaneTilt: <PaperPlaneTilt />,
   PasswordCheck: <PasswordCheck />,
   PencilSimple: <PencilSimple />,
