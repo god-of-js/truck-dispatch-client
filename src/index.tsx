02f2393c70432c43bc682fb4835883de5d30843e
Victorumeh <umehvictor28@gmail.com>
2023-06-15 13:58:15 +0100

    chore(): changed the position of the Toast to top-center

--- src/index.tsx
+++ src/index.tsx
@@ -24,7 +24,7 @@ root.render(
       <Suspense fallback={<Loader />}>
         <RouterProvider router={router} />
       </Suspense>
-      <Toaster position="bottom-right" reverseOrder={true} />
+      <Toaster position="top-center" reverseOrder={true} />
     </Provider>
   </React.StrictMode>,
 );
