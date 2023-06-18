94283ae291c87bc95b6ab07e089a16cec9924b53
god-of-js <henryeze019@gmail.com>
2023-06-18 19:17:40 +0100

    chore(): changed icon in forgot password page

--- src/pages/auth/ForgotPasswordPage.tsx
+++ src/pages/auth/ForgotPasswordPage.tsx
@@ -48,7 +48,7 @@ export default function ForgotPasswordPage() {
     <div className="duo-button-container no-btn-margin-top">
       <Link to="/auth/login">
         <UiButton size="large" variant="secondary" isFullWidth>
-          <UiIcon icon="CaretLeft" />
+          <UiIcon icon="ArrowLeft" />
           Back to sign in
         </UiButton>
       </Link>
