143a6ed4eaae45f13c15b6e9356073f7ae745538
chukwuekeuzoma <chukwuekeuzoma@gmail.com>
2023-06-18 19:13:02 +0100

    chore:404 page

--- src/components/errors/PageError.tsx
+++ src/components/errors/PageError.tsx
@@ -1,35 +1,48 @@
 import React from 'react';
-import { useRouteError } from 'react-router-dom';
-import { StringLiteralLike } from 'typescript';
-
-interface ComponentError {
-  data?: string;
-  error?: string;
-  status?: number;
-  statusText?: string;
-}
+import styled from 'styled-components';
+import UiIcon from 'ui/UiIcon';
 
 interface Props {
-  error: ComponentError;
+  children?: React.ReactNode;
 }
 
-const ErrorPage: React.FC<Props> = ({ error }) => {
-  console.error(error);
-
+export default function ErrorPage({ children }: Props) {
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>
-          {error.status} {error.statusText}
-        </i>
-      </p>
-    </div>
+    <ErrorPageContainer id="error-page">
+      <div>
+        <h1>404</h1>
+        <p className="error-text-p">Page not found</p>
+        <p className="error-text-w">We are working on it!!!</p>
+        <div>
+          <UiIcon icon="NotFound" size="350" />
+        </div>
+        <div>{children}</div>
+      </div>
+    </ErrorPageContainer>
   );
-};
-
-export default function useErrorPage() {
-  const error = useRouteError() as ComponentError;
-  return <ErrorPage error={error} />;
 }
+
+const ErrorPageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  h1 {
+    font-weight: 900;
+    font-size: 32px;
+    line-height: 37px;
+  }
+
+  .error-text-p {
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 28px;
+  }
+
+  .error-text-w {
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 21px;
+  }
+`;
