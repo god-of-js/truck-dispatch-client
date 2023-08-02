7e0fbce9fbf8737c872e58597263abc9fcd32807
god-of-js <henryeze019@gmail.com>
2023-08-02 23:59:26 +0100

    chore(): unused logs removed

--- src/routes/allowNavigationFunctions.ts
+++ src/routes/allowNavigationFunctions.ts
@@ -1,7 +1,6 @@
 import { isTokenValid } from 'utils/helpers';
 
 export function resetPasswordAccessChecks(searchUrl: string) {
-  console.log(searchUrl);
   const searchParams = new URLSearchParams(searchUrl);
 
   const token = searchParams.get('token');
