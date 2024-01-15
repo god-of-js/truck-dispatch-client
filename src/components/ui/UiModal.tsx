7d6ccc2f0e2d96101f7189211ee62c54f6c84a31
Umeh Victor <umehvictor28@gmail.com>
2024-01-15 21:08:41 +0100

    Refactor positionStyling function in UiModal.tsx

--- src/components/ui/UiModal.tsx
+++ src/components/ui/UiModal.tsx
@@ -97,28 +97,6 @@ function positionStyling({ position, size }: CardProps) {
       }
     `;
   }
-  if (position === 'center') {
-    return `
-    position: fixed;
-    margin: auto;
-    width: 80%;
-    border-radius: ${pxToRem(16)};
-    
-    
-    .modal-header h2 {
-      text-align: center;
-      flex-grow: 1;
-      margin-left: 40px !important;
-    }
-    @media only screen and (min-width: ${sizes.tabletLargeWidth}) {
-      position: static;
-      width: 70%;
-    }
-    @media only screen and (min-width: ${sizes.laptopSmallWidth}) {
-      width: 50%;
-    }
-    `;
-  }
 
   return `
     position: static;
